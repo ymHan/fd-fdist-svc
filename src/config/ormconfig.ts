abad92f339c5decef5cf82b8f1046dd5d06d0e89
ymHan <onyxsard@gmail.com>
2024-08-01 12:28:24 +0900

    sql init

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -13,14 +13,5 @@ export = [
     password: process.env.DEFAULT_DB_PASSWORD,
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
-  } as TypeOrmModuleOptions,
-  {
-    name: process.env.SECOND_DB_NAME,
-    type: process.env.SECOND_DB_TYPE,
-    host: process.env.SECOND_DB_HOST,
-    port: parseInt(process.env.SECOND_DB_PORT, 10),
-    database: process.env.SECOND_DATABASE,
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: false,
-  } as TypeOrmModuleOptions,
+  } as TypeOrmModuleOptions
 ];
