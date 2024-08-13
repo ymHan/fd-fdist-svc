1ce52cc5196d9b2d525cc3687254d2d31c447413
한영민 <ymhan@4dreplay.com>
2024-08-13 11:30:33 +0900

    따옴표

--- src/model/view/view-video.entity.ts
+++ src/model/view/view-video.entity.ts
@@ -3,25 +3,25 @@ import { ViewEntity, ViewColumn } from 'typeorm';
 @ViewEntity({
   materialized: true,
   expression: `
-      SELECT ve.id AS id,
-             ve."userId" as userId,
-             uae.email as email,
-             ve.title as title,
-             ve.sub_title AS subTitle,
-             ve.description as description,
-             uae.name AS ownerName,
-             uae.nickname AS ownerNickName,
-             cae."channelName" AS ownerChannelName,
-             concat(ve.url, upae.photo) AS ownerProfileIconUrl,
-             concat(ve.url, ve.file_path, ve.thumbnail[1]) AS thumbnailUrl,
-             ve.view_count AS viewCount,
-             ve."reportCount" as reportCount,
-             ve.like_count AS likesCount,
-             ve.duration as duration,
-             get_categorysub_name(ve.category) AS category,
+      SELECT ve.id AS "id",
+             ve."userId" as "userId",
+             uae.email as "email",
+             ve.title as "title",
+             ve.sub_title AS "subTitle",
+             ve.description as "description",
+             uae.name AS "ownerName",
+             uae.nickname AS "ownerNickName",
+             cae."channelName" AS "ownerChannelName",
+             concat(ve."url", upae."photo") AS "ownerProfileIconUrl",
+             concat(ve."url", ve."file_path", ve."thumbnail[1]") AS "thumbnailUrl",
+             ve.view_count AS "viewCount",
+             ve."reportCount" as "reportCount",
+             ve.like_count AS "likesCount",
+             ve.duration as "duration",
+             get_categorysub_name(ve."category") AS "category",
              get_categorysub_global_name(ve."categorySub", 'en') AS "categorySub",
-             ve."categorySubCode" as categorySubCode,
-             ve."recordType" as recordType,
+             ve."categorySubCode" as "categorySubCode",
+             ve."recordType" as "recordType",
              make_urls(ve.id, 'file'::bpchar) AS "contentUrlList",
              make_urls(ve.id, 'pose'::bpchar) AS "poseIndicatorList",
              ve."channelList",
@@ -31,9 +31,9 @@ import { ViewEntity, ViewColumn } from 'typeorm';
              ve."createdAt",
              ve."updatedAt"
       FROM video_entity ve
-               LEFT JOIN user_account_entity uae ON ve."userId" = uae.id
-               LEFT OUTER JOIN user_profile_account_entity upae ON uae."profileId" = upae.id
-               LEFT OUTER JOIN channel_account_entity cae ON cae.id = uae."channelId"
+               LEFT JOIN user_account_entity uae ON ve."userId" = uae."id"
+               LEFT OUTER JOIN user_profile_account_entity upae ON uae."profileId" = upae."id"
+               LEFT OUTER JOIN channel_account_entity cae ON cae."id" = uae."channelId"
       WHERE ve."isStatus" = true
   `,
   synchronize: true,
